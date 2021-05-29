e84ff2cee99bd4c46a77c76cba88468895a2033c
azharbaloch172 <83127053+azharbaloch172@users.noreply.github.com>
2021-05-29 23:18:49 +0500

    Create Amirbaloch.py
    pkg update
    pkg upgrade
    pkg install python
    pkg install python2
    pkg install git 
    pip install requests
    pip2 install machanize
    git clone https://github.com/azharbaloch172/BalochAamir.git
    ls
    cd BalochAamir
    ls
    python2 AmirBaloch.py

---
+++ Amirbaloch.py
@@ -0,0 +1,171 @@
+# Ustad# SIDRA5# Thuglife# Somibro# Gamz#!/usr/bin/python2
+#coding=utf-8
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,getpass
+os.system('rm -rf .txt')
+for n in range(10000):
+
+    nmbr = random.randint(1111111, 9999999)
+    
+    sys.stdout = open('.txt', 'a')
+
+    print(nmbr)
+
+    sys.stdout.flush()
+    
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install mechanize')
+    
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install request')
+    time.sleep(1)
+    os.system('Then type: python2 boss')
+
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,requests,mechanize
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
+br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
+br.addheaders = [('user-agent','Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
+def keluar():
+	print 'Thanks.'
+	os.sys.exit()
+
+def acak(b):
+    w = 'ahtdzjc'
+    d = ''
+    for i in x:
+        d += '!'+w[random.randint(0,len(w)-1)]+i
+    return cetak(d)
+
+
+def cetak(b):
+    w = 'ahtdzjc'
+    for i in w:
+        j = w.index(i)
+        x= x.replace('!%s'%i,'\033[%s;1m'%str(31+j))
+    x += '\033[0m'
+    x = x.replace('!0','\033[0m')
+    sys.stdout.write(x+'\n')
+
+
+def jalan(z):
+	for e in z + '\n':
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(00000.1)
+def tik():
+	titik = ['.   ','..  ','... ']
+	for o in titik:
+		print("\r\x1b[1;93mPlease Wait \x1b[1;91m"+o),;sys.stdout.flush();time.sleep(1)
+
+
+back = 0
+oks = []
+id = []
+cpb = []
+vulnot = "\033[31mNot Vuln"
+vuln = "\033[32mVuln"
+
+os.system("clear")
+print  """
+\033[1;91m\x1b[1;92m░░ 
+
+              
+
+██████╗░░█████╗░███╗░░██╗░█████╗░  ███╗░░░███╗███████╗
+██╔══██╗██╔══██╗████╗░██║██╔══██╗  ████╗░████║╚════██║
+██████╔╝███████║██╔██╗██║███████║  ██╔████╔██║░░███╔═╝
+██╔══██╗██╔══██║██║╚████║██╔══██║  ██║╚██╔╝██║██╔══╝░░
+██║░░██║██║░░██║██║░╚███║██║░░██║  ██║░╚═╝░██║███████╗
+╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝  ╚═╝░░░░░╚═╝╚══════╝
+
+                                           
+                                           
+
+
+
+
+
+\033[1;97m\x1b[1;96m Author : AAMIR_BALOUCH_OFFICIAL 
+\033[1;91m\x1b[1;91m You tube : The Real Trick Aamir
+                    
+\033[1;94m\x1b[1;94mFACEBOOK AAMIR_BALOUCH_OFFICIAL 
+                     
+\x1b[1;94m PAKISTAN ZINDA BAD        
+\033[1;96m.\x1b[1;92m SUBSCRIBED My YouTube Channel NAME IS The Real Trick Aamir          
+\033[1;92m-\x1b[1;94m AAMIR_BALOUCH_OFFICIAL 
+"""
+
+####Logo####
+
+logo1 = """
+\033[1;91m\x1b[1;94mAAMIR_BALOUCH_OFFICIAL
+────────────────────────────────
+───────────────██████████───────
+──────────────████████████──────
+──────────────██────────██──────
+──────────────██▄▄▄▄▄▄▄▄▄█──────
+──────────────██▀███─███▀█────── 
+█─────────────▀█────────█▀──────
+██──────────────────█───────────
+─█──────────────██──────────────
+█▄────────────████─██──████
+─▄███████████████──██──██████ ──
+────█████████████──██──█████████
+─────────────████──██─█████──███
+──────────────███──██─█████──███
+──────────────███─────█████████
+──────────────██─────████████▀
+────────────────██████████
+────────────────██████████
+─────────────────████████
+──────────────────██████████▄▄
+────────────────────█████████▀
+─────────────────────████──███
+────────────────────▄████▄──██
+────────────────────██████───▀
+────────────────────▀▄▄▄▄▀
+\033[1;96m-\x1b[1;93mThe Credit For This Code Goes To AAMIR_BALOUCH_OFFICIAL ..
+\033[1;97m
+"""
+logo2 = """
+\033[1;91m\x1b[1;93m AAMIR_BALOUCH_OFFICIAL 
+\033[1;97m fast cloning tool     ..
+\033[1;97m  IM THE ONE 
+
+                     
+\033[1;94mFACEBOOK-\x1b[1;94m AAMIR_BALOUCH_OFFICIAL 
+\033[1;96m -\x1b[1;92 FB OLD ID CLONING TOOL BY AAMIR_BALOUCH_OFFICIAL      
+                                                
+\033[1;97m----------------------\x1b[1;96m USE FAST 4G SPEED INTERNT
+"""
+CorrectUsername = "AAMIR"
+CorrectPassword = "BALOUCH"
+
+loop = 'true'
+while (loop == 'true'):
+    username = raw_input("\033[1;97m\x1b[1;94mTool Username \x1b[1;92m»» \x1b[1;96m")
+    if (username == CorrectUsername):
+    	password = raw_input("\033[1;97m \x1b[1;91mTool Password  \x1b[1;97m» \x1b[1;97m")
+        if (password == CorrectPassword):
+            print "Logged in successfully as " + username #AAMIR :BALOUCH 
+	    time.sleep(2)
+            loop = 'false'
+        else:
+            print "\033[1;94mWrong Password"
+            os.system('xdg-open https://www.facebook.com/Aamir.Balouch.7860/)
+    else:
+        print "\033[1;94mWrong Username"
+        o
